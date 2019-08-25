129b365cc3f7030c0bcfd78aa5f53f5e2a43bfba
Mike Williams <573678+pushups@users.noreply.github.com>
2019-08-25 12:32:50 -0700

    Add insert function

--- src/hash_table.c
+++ src/hash_table.c
@@ -56,3 +56,18 @@ int ht_get_hash(const char *s, const int num_buckets, const int attempt) {
   const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
   return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+void ht_insert(ht_hash_table *ht, const char* key, const char* value) {
+  ht_item* item = ht_new_item(key, value);
+  int index = ht_get_hash(item->key, ht->size, 0);
+  ht_item* cur_item = ht->items[index];
+  int i = 1;
+  while (cur_item != NULL) {
+    index = ht_get_hash(item->key, ht->size, i);
+    cur_item = ht->items[index];
+    i++;
+  }
+
+  ht->items[index] = item;
+  ht->count++;
+}
--- src/hash_table.h
+++ src/hash_table.h
@@ -13,3 +13,7 @@ ht_hash_table* ht_new();
 void ht_del_hash_table(ht_hash_table* ht);
 int ht_hash(const char *s, const int a, const int m);
 int ht_get_hash(const char *s, const int num_buckets, const int attempt);
+
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
